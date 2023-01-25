361b7c9e217384357f0d1d9bef9f44feeb64e635
mahrez1 <59502081+mahrez1@users.noreply.github.com>
2023-01-25 09:32:48 +0100

    cart status home page

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -22,7 +24,7 @@ export class ProductListComponent implements OnInit {
 
 
 
-  constructor(private productService : ProductService , private route : ActivatedRoute) { }
+  constructor(private productService : ProductService , private route : ActivatedRoute, private cartService : CartService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(()=>{this.listProducts()
@@ -103,7 +105,8 @@ export class ProductListComponent implements OnInit {
   addToCard(theProduct : Product)
   {
     console.log(`add to card : ${theProduct.name}`) ;
-    //to do
+    const theCartItem = new CartItem(theProduct) ;
+    this.cartService.addToCart(theCartItem) ;
   }
 
 
